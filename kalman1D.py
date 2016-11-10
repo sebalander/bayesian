e_inf formula corrected

kalman1D.py
@@ -14,21 +14,21 @@ import pykalman as pk
 # defino sistema sin excitación
 A = 1
 H = 1
-Q = 0.1
+Q = 1
 R = 1
-n = 50 # cantidad de pasos
+n = 100 # cantidad de pasos
 ite = np.arange(n)
 
 # condiciones iniciales y lista de estados para todo tiempo
 x = np.zeros(n)
-P = np.ones(n)+1
+P = np.ones(n)*3
 xp = np.zeros(n)
 Pp = np.zeros(n)
 
 # "realidad" y mediciones
-v = 1 # velocidad
+v = 10 # velocidad
 X = ite * v
-ruido = np.random.rand(n) - 0.5
+ruido = (np.random.randn(n) - 0.5) * R
 z = X + ruido
 
 # %% bucle
@@ -60,20 +60,23 @@ pt.errorbar(ite,x,yerr=sig,label='estimacion')
 pt.legend(loc='best')
 
 # %% plot errores
-Pinf = (-Q + np.sqrt(Q**2 + 4 + R * Q)) / 2
+Pinf = (-Q + np.sqrt(Q**2 + 4 * R * Q)) / 2
 sInf = np.sqrt(Pinf)
 
-eInf = - v * (Pinf + Q + R) / (Pinf + Q)
+eInf = - v * R / (Pinf + Q)
 
 pt.figure()
 pt.title('desv estandar y erroes')
 
 pt.plot([0,n],[sInf,sInf],'--b',label=r'$\sqrt{P_{inf}}$')
 pt.plot([0,n],[-sInf,-sInf],'--b')
+
 pt.plot(sig,'-b',linewidth=2,label=r'$\sqrt{P}$')
 pt.plot(-sig,'-b',linewidth=2)
 pt.fill_between(ite,-sig,sig,facecolor='b',alpha=0.4)
 
 pt.plot([0,n],[eInf,eInf],'--k',label=r'$e_{inf}$')
-pt.plot(e,'-k',linewidth=2,label=r'$e$')
+pt.plot(ite,e,'-k',linewidth=2,label=r'$e$')
+
+
 pt.legend(loc=1)